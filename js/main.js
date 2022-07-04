acb0074b27ba6b330a386583c47e0a222437fa63
Stefano Morgia <ste.morgia@gmail.com>
2022-07-04 14:55:32 +0200

    prove js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const età = parseInt (prompt('Inserisci la tua età'));
+const kilometri = parseInt (prompt('Inserisci numero kilometri da percorrere'))
+const tariffa = 0.21 
+const prezzo = tariffa * kilometri
+if (età < 18){
+    const prezzo
+}
+alert ('Il prezzo è di ' + prezzo + ' Euro')
